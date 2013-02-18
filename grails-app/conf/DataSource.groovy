c220c3690df8402e516586aeb4d750cb5c20176e
b1006433 <b1006433@my.shu.ac.uk>
2013-02-18 16:34:52 +0000

    Changed it from rather than using a internal memory of grails that only stores data per session to mysql database using utf8 (minor mistake on previous commit)

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,12 +17,6 @@ environments {
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
             driverClassName = "com.mysql.jdbc.Driver"
             dialect=org.hibernate.dialect.MySQL5Dialect
             username = "webarch"
@@ -31,6 +25,22 @@ environments {
             pooled = true
         }
 
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
         }
     }
 }
